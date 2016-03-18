f4753e8092405e27ed5bcf9d7df083ae0f4870c9
Francis Brito <francis@instacarro.com>
2016-03-17 22:20:49 -0400

    Add gulp tasks documentation.

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ gulp.task('browser-sync', function(done) {
 });
 
 // Clean
+// TODO: This task does nothing. Remove it.
 gulp.task('clean', function(cb) {
   del(['css'], cb);
 });
@@ -75,6 +76,6 @@ gulp.task('watch', function() {
 
   gulp.watch('assets/scss/*.scss', ['styles']);
 
+  // TODO: Remove this.
   gulp.watch('views/*.html');
-
 });
